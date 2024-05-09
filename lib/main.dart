e486e8503998c3b802814edbbb552219c6910b6d
Pudding(Lloyd) <johnlloyddesape@gmail.com>
2024-05-09 11:24:54 +0800

    added static design for the appbar and body

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,53 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return  MaterialApp(
       home: Scaffold(
+        backgroundColor: const Color.fromARGB(255, 231, 248, 232),
+        appBar: AppBar(
+          leading: const Icon(Icons.energy_savings_leaf, color: Colors.white),
+          title: const Text('Plant Health Monitoring App',
+          style: TextStyle(color: Colors.white),
+          ),
+          backgroundColor: Colors.green,
+        ),
+          body: const Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children:[
+              Text(
+                'Moisture Level', // Replace with the actual percentage text
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: Colors.green),
+              ),
+              SizedBox(height: 30),
+              SizedBox(
+                width: 200, // Set the width to make it bigger
+                height: 200, // Set the height to make it bigger
+                child: Stack(
+                  children: <Widget>[
+                    Center(
+                      child: SizedBox(
+                        width: 200, // Set the width of CircularProgressIndicator
+                        height: 200, // Set the height of CircularProgressIndicator
+                        child: CircularProgressIndicator(
+                          value: .3, // Replace with the actual percentage value
+                          valueColor: AlwaysStoppedAnimation<Color>(Colors.green),
+                          backgroundColor: Colors.white,
+                          strokeWidth: 10,
+                          strokeCap: StrokeCap.round
+                        ),
+                      ),
+                    ),
+                    Center(
+                      child: Text(
+                        '30%', // Replace with the actual percentage text
+                        style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: Colors.green),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
         bottomNavigationBar: Container(
           color: Colors.green,
           child:  const Padding(
@@ -34,7 +81,7 @@ class MainApp extends StatelessWidget {
                   ),
                 GButton(
                  //light icon
-                 icon: Icons.light_mode_outlined,
+                 icon: Icons.sunny_snowing,
                   text: 'Light'
                 ),
               ]
