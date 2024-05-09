d8746bb6033ea332afe61c3422ca2165247d413f
Pudding(Lloyd) <johnlloyddesape@gmail.com>
2024-05-09 09:34:07 +0800

    added static google navigation bar design

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,12 +10,39 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return  MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        bottomNavigationBar: Container(
+          color: Colors.green,
+          child:  const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 15.0, vertical: 18),
+            child:  GNav(
+              backgroundColor: Colors.green,
+              color: Colors.white,
+              activeColor: Colors.green,
+              tabBackgroundColor: Colors.white,
+              padding: EdgeInsets.all(16),
+              gap: 8,
+              tabs:  [
+                GButton(
+                  icon: Icons.water_drop_outlined,
+                  text: 'Moisture'
+                  ),
+                GButton(
+                  icon: Icons.thermostat,
+                  text: 'Temperature'
+                  ),
+                GButton(
+                 //light icon
+                 icon: Icons.light_mode_outlined,
+                  text: 'Light'
+                ),
+              ]
+            ),
+          ),
         ),
       ),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
